5636153676802088e2c7cb82cb07d134ed4b77f7
SajeedAninda <sajaninda@gmail.com>
2023-07-28 23:09:26 +0600

    Commit

---
+++ Milestone-4/Module-16/variable.js
@@ -0,0 +1,18 @@
+// Js Variable 
+
+// NUMERIC TYPE 
+var price = 11;
+var temperature = 38;
+var weight = 78;
+var money = 55;
+var friends = 5;
+
+// STRING TYPE 
+var name = "Aninda";
+var age = 23;
+var address = "Senpara";
+
+// BOOLEAN TYPE 
+
+var serious = true;
+var isFullMarks = false;

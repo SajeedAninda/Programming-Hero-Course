acb147616982be7244b2f2c479723070b9649fce
SajeedAninda <sajaninda@gmail.com>
2023-07-29 18:42:05 +0600

    Commit

---
+++ Milestone-4/Module-16/problem.js
@@ -0,0 +1,28 @@
+// // let moneyReceived = 1000;
+// // let applePrice = 200;
+// // let orangePrice = 300;
+// // let totalCost = (applePrice + orangePrice);
+// // let moneyReturn = (moneyReceived - totalCost);
+// // console.log("The total returned amount is " + moneyReturn);
+
+
+// // let Mathematic = 75.25;
+// // let biology = 65;
+// // let physics = 80;
+// // let chemistry = 35.45;
+// // let Bangla = 99.50;
+// // let totalMarks = (Mathematic+biology+physics+chemistry+Bangla);
+// // let totalSub=5;
+// // let Average = totalMarks/totalSub;
+// // console.log(Average.toFixed(2));
+
+// let line1 = "I am going to be";
+// let line2 = "an awesome web developer";
+// console.log(line1.concat(" ", line2));
+// console.log(line1 + " " + line2);
+
+
+let numberGiven = 119;
+let dividedBy = 5;
+let modulo = numberGiven % dividedBy;
+console.log(modulo);
---
+++ Milestone-4/Module-16/type.js
@@ -0,0 +1,18 @@
+// let firstName = "Aninda";
+// let age = 23;
+// let single = true;
+// console.log(typeof (firstName));
+// console.log(typeof (age));
+// console.log(typeof (single));
+
+// let decimalNum=3.1416;
+// let fixedNum=decimalNum.toFixed(2);
+// // console.log(fixedNum , typeof fixedNum);
+// fixedNum=parseFloat(fixedNum);
+// console.log(fixedNum, typeof fixedNum);
+
+
+let a = 19;
+let b = 7;
+let modulo = a % b;
+console.log(modulo);

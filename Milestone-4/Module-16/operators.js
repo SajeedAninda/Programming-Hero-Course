a08f4987081857b0b06344fcab89747c01b72327
SajeedAninda <sajaninda@gmail.com>
2023-07-29 12:54:30 +0600

    Commit

---
+++ Milestone-4/Module-16/operators.js
@@ -0,0 +1,16 @@
+let onionPrice = 50;
+let eggPrice = 40;
+let totalSpent = (onionPrice + eggPrice);
+// console.log(totalSpent);
+let priceDifference = (onionPrice - eggPrice);
+// console.log(priceDifference);
+
+let orangePrice = 40;
+let orangeRequired = 10;
+let totalOrangePrice = (orangePrice * orangeRequired);
+// console.log(totalOrangePrice);
+
+let totalMoney = 500;
+let totalPeople = 10;
+let eachPerson = totalMoney / totalPeople;
+console.log(eachPerson);

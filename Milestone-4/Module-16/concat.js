02e81d004467bd72038d5367edc1ef85b1626f1e
SajeedAninda <sajaninda@gmail.com>
2023-07-29 13:50:03 +0600

    Commit

---
+++ Milestone-4/Module-16/concat.js
@@ -0,0 +1,11 @@
+let firstName = "Sajeed";
+let middleName = "Enayet";
+let thirdName = "Aninda";
+let fullName = firstName + middleName + thirdName;
+// console.log(fullName);
+fullName = firstName + " " + middleName + " " + thirdName;
+// console.log(fullName);
+let concattt = firstName.concat(middleName, thirdName);
+console.log(concattt);
+concattt = firstName.concat(" ", middleName, " ", thirdName);
+console.log(concattt);
---
+++ Milestone-4/Module-16/parse.js
@@ -0,0 +1,17 @@
+// let num1= 10;
+// let num2="20";
+// let num3=num1+num2;
+// console.log(num3);
+// num2=parseInt(num2);
+// num3=num1+num2;
+// console.log(num3);
+
+
+let num1 = 3.53;
+let num2 = "566";
+let num3 = num1 + num2;
+console.log(num3);
+num1 = parseFloat(num1);
+num2 = parseInt(num2);
+num3 = num1 + num2;
+console.log(num3);

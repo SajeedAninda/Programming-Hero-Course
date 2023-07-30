aee64329dfa35bfce877da8fad1f67d83a63aa5c
SajeedAninda <sajaninda@gmail.com>
2023-07-30 21:42:59 +0600

    Commit

--- Milestone-4/Module-17/conditional.js
+++ Milestone-4/Module-17/conditional.js
@@ -1,8 +1,8 @@
-let age = 13;
-if (age > 18) {
-    console.log("You are an adult");
-}
-else {
-    console.log("You are a minor");
-}
+// let age = 13;
+// if (age > 18) {
+//     console.log("You are an adult");
+// }
+// else {
+//     console.log("You are a minor");
+// }
 
---
+++ Milestone-4/Module-17/logical-conditions.js
@@ -0,0 +1,32 @@
+// let hungry = true;
+// let budget = 450;
+// let biriyaniPrice = 350;
+
+// if (hungry == true && budget > biriyaniPrice) {
+//     console.log("We will have biriyani");
+// }
+// else {
+//     console.log("Stay content with Daal Bhaat");
+// }
+
+// let salary = 75000;
+// let single = true;
+// let cars = 1;
+
+// if (salary > 50000 && cars >= 1 && single == true) {
+//     console.log("You can marry");
+// }
+// else {
+//     console.log("Wait for some time");
+// }
+
+// let salary = 40000;
+// let single = true;
+// let cars = 1;
+
+// if (single == true && (salary >= 50000 || cars >= 1)) {
+//     console.log("You can marry");
+// }
+// else {
+//     console.log("Stay Single");
+// }

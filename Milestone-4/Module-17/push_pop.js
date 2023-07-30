48cec26f9dd4c2cdd3ec45a117f2335c40c2648f
SajeedAninda <sajaninda@gmail.com>
2023-07-30 13:31:23 +0600

    Commit

---
+++ Milestone-4/Module-17/push_pop.js
@@ -0,0 +1,16 @@
+// let nums = [101, 93, 53, 44, 28, 35, 46, 99];
+// // // nums.push(333);
+// // // console.log(nums);
+
+// let friends = ["Aninda" , "Anik" , "Zidan" , "Osi" , "Sami", "Romel"];
+// // console.log(friends);
+// // friends.push("Ahnaf");
+// // console.log(friends);
+// let element=new Array();
+// element.push(nums.pop());
+// element.push(nums.pop());
+// element.push(nums.pop());
+// element.push(nums.pop());
+
+// console.log(nums);
+// console.log(element);

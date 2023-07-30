374846d6b997a99efd13a437feaeca791f7fe10f
SajeedAninda <sajaninda@gmail.com>
2023-07-30 16:11:10 +0600

    Commit

---
+++ Milestone-4/Module-17/comparison.js
@@ -0,0 +1,25 @@
+// console.log(5>6);
+// console.log(6>5);
+// console.log(5>6);
+// console.log(6<5);
+
+// console.log(6 == 5);
+// console.log(6 !== 5);
+// console.log(6 === 5);
+// console.log("5" == 5);
+// console.log("5" === 5);
+
+// console.log(14>=6);
+// console.log(14>=16);
+// console.log(5<=6);
+// console.log(7<=6);
+
+// let num1 = 5;
+// let num2 = 6;
+// let num3 = "6";
+// if(num2 == num3 || num2 === num3){
+//     console.log(True);
+// }
+// else{
+//     console.log(false);
+// }

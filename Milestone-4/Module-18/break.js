eaa77cb130cbf53b6a0598dcfaf2a29d80659ffb
SajeedAninda <sajaninda@gmail.com>
2023-07-31 21:59:58 +0600

    Commit

--- Milestone-4/Module-18/break.js
+++ Milestone-4/Module-18/break.js
@@ -1,15 +1,23 @@
-// let items = ["Bottle", "Mouse", "Sunglass", "Pen"];
-// for (let i = 0; i < (items.length); i++) {
-//     let item = items[i];
-//     if (items == "Pen") {
-//         break;
+// // let items = ["Bottle", "Mouse", "Sunglass", "Pen"];
+// // for (let i = 0; i < (items.length); i++) {
+// //     let item = items[i];
+// //     if (items == "Pen") {
+// //         break;
+// //     }
+// //     console.log(item);
+// // }
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i == 5) {
+//         continue;
 //     }
-//     console.log(item);
+//     console.log(i);
 // }
 
-for (let i = 0; i <= 10; i++) {
-    if (i == 5) {
-        continue;
-    }
-    console.log(i);
-}
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         break; // The loop will terminate when i is equal to 5
+//     }
+//     console.log(i);
+// }

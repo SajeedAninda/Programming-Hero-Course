a9c39c98ab0e54a03c43ff57580f3721cddc95e5
SajeedAninda <sajaninda@gmail.com>
2023-07-31 12:35:29 +0600

    Commit

---
+++ Milestone-4/Module-18/while.js
@@ -0,0 +1,13 @@
+// let roastGiven = 0;
+// while (roastGiven < 7) {
+//     console.log("I need a roast");
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+
+// let greetings = 0;
+// while (greetings < 10) {
+//     console.log("Hello, Nihao");
+//     greetings++;
+// }
+

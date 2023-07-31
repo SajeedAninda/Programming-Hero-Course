ea25a7bb6187dd25bfd00988d24eae2f2deb9c54
SajeedAninda <sajaninda@gmail.com>
2023-07-31 14:06:41 +0600

    Commit

---
+++ Milestone-4/Module-18/evenodd.js
@@ -0,0 +1,18 @@
+// let num = 0;
+// while (num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// // let number = 1;
+// // while (number < 100) {
+// //     if (number % 2 === 0) {
+// //         console.log(number + " is even");
+// //     }
+// //     else {
+// //         console.log(number + " is odd");
+// //     }
+// //     number++;
+// // }
+
---
+++ Milestone-4/Module-18/for.js
@@ -0,0 +1,24 @@
+// for (let num = 0; num <= 100; num++) {
+//     if (num % 2 === 0) {
+//         console.log(num + " is even");
+//     }
+//     else {
+//         console.log(num + " is odd");
+//     }
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i = i + 2) {
+//     console.log(i);
+// }
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+
+console.log('Sum:', sum); // Output: Sum: 55

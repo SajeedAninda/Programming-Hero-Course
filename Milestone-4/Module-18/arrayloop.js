977cf2a4593d948c23cbb1644f6936c90167c488
SajeedAninda <sajaninda@gmail.com>
2023-07-31 16:05:12 +0600

    Commit

---
+++ Milestone-4/Module-18/arrayloop.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let arr = ["mango", "banana", "orange", "apple", "jackfruit", "Pineapple"];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// let items = ["Bottle", "Mouse", "Sunglass", "Pen"];
+// for (let i = 0; i < (items.length); i++) {
+//     console.log(items[i]);
+// }

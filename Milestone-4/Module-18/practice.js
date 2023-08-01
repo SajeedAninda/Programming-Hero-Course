44467b3760924f34654160e25fa8ec7f456058c4
SajeedAninda <sajaninda@gmail.com>
2023-08-01 16:06:30 +0600

    Commit

---
+++ Milestone-4/Module-18/practice.js
@@ -0,0 +1,70 @@
+// // // // // // 1. Display ajke amar mon bhalo nei for 39 times 
+// // // // // for(i=1;i<=39;i++){
+// // // // //     console.log(i+". Ajke amar mon bhalo nei.")
+// // // // // }
+
+
+// // // // // 2. Display Numbers Between 58 to 98 
+// // // // for (let i = 58; i <= 98; i++) {
+// // // //     console.log(i);
+// // // // }
+
+
+// // // // 3. Show All Even Numbers from 412 to 456 
+// // // for (i = 412; i <= 456; i++) {
+// // //     if (i % 2 == 0) {
+// // //         console.log(i);
+// // //     }
+// // // }
+
+
+// // // 4. Show All ODD Numbers between 581 to 623
+// // for (i = 581; i <= 623; i++) {
+// //     if (i % 2 == 1) {
+// //         console.log(i);
+// //     }
+// // }
+
+// // 5. Difference between while loop and for loop 
+// // The main difference between a while loop and a for loop in JavaScript lies in their syntax and usage, though they are both used for repetitive execution of a block of code.
+// // While Loop- 
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++
+// }
+// // For Loop 
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+// // BOTH OF THESE PRINTS THE SAME RESULT BUT HAVE SLIGHTLY DIFFERENT SYNTAXES
+
+// 6. Declare an array for all the topics that you have learned in javascript and output them.
+// let learnt = ["Variables", "Data Types", "Operators", "Comparators", "Conditionals", "Loops", "And SO ON"];
+// for (i = 0; i <= learnt.length; i++) {
+//     console.log(learnt[i]);
+// }
+
+// 7. Create an array with all mobile phone brands. Display them using while loop 
+// let phoneBrands = ["Apple", "Samsung", "Xiaomi", "Realme", "Oneplus", "Pixel", "Sony", "Huawei", "Vivo", "Oppo", "Honor", "Nokia", "Techno", "Zte", "And So On"]
+// let i = 0;
+// while (i < phoneBrands.length) {
+//     console.log(phoneBrands[i]);
+//     i++;
+// }
+
+// 8. Run a loop from 30 to 86. This Loop will stop if the values get higher than 44
+// for (i = 30; i <= 86; i++) {
+//     if (i > 44) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// 9. Write the prices of the books that you HashChangeEvent. Display the prices if price the price is less than 200.
+// let price = [180, 480, 133, 90, 60, 600, 120, 203, 198, 177, 900, 100, 122, 1456, 135, 189, 30, 240, 580, 40];
+// for (i = 0; i <= price.length; i++) {
+//     if (price[i] < 200) {
+//         console.log(price[i]);
+//     }
+// }

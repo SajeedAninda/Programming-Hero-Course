efe1ce756813229278f8245ceae2dc2c926cc798
SajeedAninda <sajaninda@gmail.com>
2023-08-01 14:07:34 +0600

    Commit

---
+++ Milestone-4/Module-18/conceptual.js
@@ -0,0 +1,29 @@
+// // // let num="1.567";
+// // // floatedNum=parseFloat(num);
+// // // console.log(floatedNum.toFixed(2));
+
+// let arr = [1, 3, 4, 6, 1, 5, 6, 2];
+// // console.log(typeof arr);
+// // let indexedArr = arr.indexOf(6);
+// // arr[indexedArr] = 66;
+// // console.log(arr);
+
+// let arr3 = arr[3];
+// let arr4 = arr[4];
+// let addedArr = arr3 + arr4;
+// arr[5] = addedArr;
+// console.log(arr);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("I am sorry")
+// }
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// for (let i = 0; i <= arr.length; i++) {
+//     if (arr[i] == 50) {
+//         continue;
+//     }
+//     else {
+//         console.log(arr[i]);
+//     }
+// }

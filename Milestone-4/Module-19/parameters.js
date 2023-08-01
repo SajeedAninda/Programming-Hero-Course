b976bef36c155cd25bb24a2a48c0fa7346013763
SajeedAninda <sajaninda@gmail.com>
2023-08-01 21:37:02 +0600

    Commit

---
+++ Milestone-4/Module-19/parameters.js
@@ -0,0 +1,11 @@
+function summation(a, b, c) {
+    sum = a + b + c;
+    console.log(sum);
+}
+summation(1, 2, 3);
+
+function traingleArea(b, h) {
+    let area = 1 / 2 * (b * h);
+    console.log(area);
+}
+traingleArea(10, 15);

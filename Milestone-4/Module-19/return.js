990ea8a1486220eb02dfbf3421bdf49252f0afa6
SajeedAninda <sajaninda@gmail.com>
2023-08-01 22:29:36 +0600

    Commit

---
+++ Milestone-4/Module-19/return.js
@@ -0,0 +1,13 @@
+function returned(a, b) {
+    let sum = a + b;
+    return (sum);
+}
+let sum = returned(4, 5);
+console.log(sum);
+
+function shingara(money, amount) {
+    let priceOfEach = money / amount;
+    return ("The price of Each Shingara is " + priceOfEach);
+}
+let price = shingara(400, 40);
+console.log(price);

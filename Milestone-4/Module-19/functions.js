e98ecd765a0d3505a8f2b940d4318c5e7c83f13e
SajeedAninda <sajaninda@gmail.com>
2023-08-01 21:17:50 +0600

    Commit

---
+++ Milestone-4/Module-19/functions.js
@@ -0,0 +1,9 @@
+function defineAge(age) {
+    console.log("My age is " + age);
+}
+defineAge(23);
+
+function bazar(spices, chicken, beef) {
+    console.log("The price for spices will be " + spices + " taka and the price for chicken and beef is " + chicken + " and " + beef + " respectively");
+}
+bazar(30, 320, 850);

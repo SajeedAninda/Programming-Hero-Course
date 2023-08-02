2209764bc5edbb1710858c2473cb9d2055a4eb42
SajeedAninda <sajaninda@gmail.com>
2023-08-02 15:17:04 +0600

    Commit

---
+++ Milestone-4/Module-19/functions_example.js
@@ -0,0 +1,24 @@
+// function average(ass1, ass2, ass3) {
+//     let totalMarks = ass1 + ass2 + ass3;
+//     let assAverage = totalMarks / 3;
+//     return assAverage;
+// }
+
+// let totalAverage = average(60, 60, 60);
+// console.log(totalAverage);
+
+// function add(num1, num2) {
+//     let summation = num1 + num2;
+//     return summation;
+// }
+// let sum1 = add(30, 40);
+// let sum2 = add(35, 60);
+// let total = add(sum1, sum2);
+// console.log(total);
+
+// function square(number) {
+//     let squared = number * number;
+//     return squared;
+// }
+// let squaredNumber = square(5);
+// console.log(squaredNumber);

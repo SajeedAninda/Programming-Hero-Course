dab2603d1cb18bb4847b3829014a579697106ec9
SajeedAninda <sajaninda@gmail.com>
2023-08-03 12:16:01 +0600

    Commit

--- Milestone-4/Module-20/sumOfArray.js
+++ Milestone-4/Module-20/sumOfArray.js
@@ -4,7 +4,7 @@ let sum = 0;
 for (let i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
 }
-console.log(sum);
+// console.log(sum);
 
 
 // DOING IN FUNCTION 
@@ -16,4 +16,21 @@ function sumOfArray(arr) {
     return sum;
 }
 
-console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+
+// FIND ODD SUMS OF AN ARRAY 
+
+function oddSums(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 1) {
+            sum = sum + arr[i];
+        }
+    }
+    return sum;
+}
+
+console.log(oddSums([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(oddSums([12, 2, 32, 44, 56, 66, 78, 82, 94, 100]));
+console.log(oddSums([5, 7, 8, 10, 45, 30]));

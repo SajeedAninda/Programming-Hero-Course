2c09c67f3eb3da1e3ee5365f173c0f76910ea237
SajeedAninda <sajaninda@gmail.com>
2023-08-04 21:28:19 +0600

    Commit

---
+++ Milestone-4/Module-21/string.js
@@ -0,0 +1,15 @@
+let str = "Aninda";
+let strLen = str.length;
+// console.log(strLen);
+// console.log(str.toLowerCase());
+
+let userName = "sajeed_aninda";
+let userInput = "Sajeed_Aninda";
+let correctUserInput=userInput.toLowerCase();
+
+if (userName === correctUserInput) {
+    console.log("Logged in Succesfully");
+}
+else {
+    console.log("Incorrect User Input");
+}

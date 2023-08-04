3e9851b621703bb076db6d7890cc219547a7153b
SajeedAninda <sajaninda@gmail.com>
2023-08-04 22:43:23 +0600

    Commit

---
+++ Milestone-4/Module-21/stringMethods.js
@@ -0,0 +1,5 @@
+let givenSentence = "Hello guys. This is Rafsan the chotobhai. Welcome back to another new Video. Ajker Video ta ektu beshi Interesting"
+let words = givenSentence.split(" ");
+console.log(words);
+let sentences = givenSentence.split(".");
+console.log(sentences);

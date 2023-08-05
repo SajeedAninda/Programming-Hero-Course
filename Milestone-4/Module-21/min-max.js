6e992db5cb3f030cc17e2b63f7424df1cf1974a4
SajeedAninda <sajaninda@gmail.com>
2023-08-05 14:52:54 +0600

    Commit

--- Milestone-4/Module-21/min-max.js
+++ Milestone-4/Module-21/min-max.js
@@ -4,8 +4,29 @@
 // }
 // console.log(max(86, 41, 78));
 
-function min(a, b, c) {
-    let smallest = Math.min(a, b, c);
-    return `The smallest number amongst ${a}, ${b} and ${c} is ${smallest}`;
+// function min(a, b, c) {
+//     let smallest = Math.min(a, b, c);
+//     return `The smallest number amongst ${a}, ${b} and ${c} is ${smallest}`;
+// }
+// console.log(min(34, 11, 66));
+
+function max(a, b, c, d, e) {
+    let largest = Math.max(a, b, c, d, e);
+    if (largest == a) {
+        return `${a} is the largest`;
+    }
+    else if (largest == b) {
+        return `${b} is the largest`;
+    }
+    else if (largest == c) {
+        return `${c} is the largest`;
+    }
+    else if (largest == d) {
+        return `${d} is the largest`;
+    }
+    else {
+        return `${e} is the largest`;
+    }
 }
-console.log(min(34, 11, 66));
+
+console.log(max(84, 55, 77, 11, 34));

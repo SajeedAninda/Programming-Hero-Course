995bf8115caf1841e30420b69039feec1c2f422a
SajeedAninda <sajaninda@gmail.com>
2023-08-05 22:18:44 +0600

    Commit

---
+++ Milestone-4/Module-22/arr.js
@@ -0,0 +1,16 @@
+let num = 12;
+let str = "Hello Friends";
+let arr = [1, 2, 4, 6, 2, 1, 45, 1, 34, 5];
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof arr);
+let check = Array.isArray(arr);
+// console.log(check);
+
+// console.log(arr.includes(4));
+// console.log(arr.includes(51));
+
+let a = [1, 2, 3, 4, 5];
+let b = [6, 7, 8, 9, 10];
+let c = a.concat(b, 11, 12, 13);
+// console.log(c);

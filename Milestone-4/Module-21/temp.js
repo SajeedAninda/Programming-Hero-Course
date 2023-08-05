1f4d862ab9f49c24376f26cd4b8e25cc6512b69b
SajeedAninda <sajaninda@gmail.com>
2023-08-05 12:55:13 +0600

    Commit

---
+++ Milestone-4/Module-21/temp.js
@@ -0,0 +1,18 @@
+// let first = 5;
+// let second = 7;
+// console.log(first, second);
+// let temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// [first, second] = [second, first];
+// console.log(first,second);
+
+let a = 5;
+let b = 7;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
+

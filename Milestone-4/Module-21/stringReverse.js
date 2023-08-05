acd837f9886bd0d1b90eea62a4fdb699f8ed4177
SajeedAninda <sajaninda@gmail.com>
2023-08-05 16:10:46 +0600

    Commit

--- Milestone-4/Module-21/stringReverse.js
+++ Milestone-4/Module-21/stringReverse.js
@@ -1,10 +1,19 @@
-function reverseString(str) {
+// function reverseString(str) {
+//     let newStr = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newStr = newStr + str[i];
+//     }
+//     return newStr;
+// }
+
+// let input = reverseString("Hello");
+// console.log(input);
+
+function anotherReverse(str) {
     let newStr = "";
     for (let i = str.length - 1; i >= 0; i--) {
         newStr = newStr + str[i];
     }
     return newStr;
 }
-
-let input = reverseString("Hello");
-console.log(input);
+console.log(anotherReverse("I am a good Boy"));

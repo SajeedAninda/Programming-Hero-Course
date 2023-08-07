9ebe9a2eaaa5dfb26e8a56e139e1cd1add31b132
SajeedAninda <sajaninda@gmail.com>
2023-08-07 14:30:00 +0600

    Commit

---
+++ Milestone-4/Module-22/takaToDollar.js
@@ -0,0 +1,13 @@
+function takaToDollar(taka) {
+    dollar = 108.35 * taka;
+    return dollar;
+}
+
+console.log(takaToDollar(500));
+
+function takaToYuan(taka) {
+    rmb = 16.50 * taka;
+    return rmb;
+}
+
+console.log(takaToYuan(2800));

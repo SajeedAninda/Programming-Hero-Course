03826896ff49e91b9f47054131ceba0ee597453f
SajeedAninda <sajaninda@gmail.com>
2023-08-07 12:09:28 +0600

    Commit

---
+++ Milestone-4/Module-22/feetToInch.js
@@ -0,0 +1,14 @@
+function feetToInch(feet) {
+    let inch = feet * 12;
+    return inch;
+}
+
+let feetInInches = feetToInch(5);
+// console.log(feetInInches);
+
+function centimeterToMeter(cm) {
+    let m = cm / 100;
+    return m;
+}
+let cmInM = centimeterToMeter(280);
+console.log(cmInM);

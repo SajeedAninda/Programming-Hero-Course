461a0b70cc92ed52ae3b35f51e23ebe104872e7e
SajeedAninda <sajaninda@gmail.com>
2023-08-12 23:51:03 +0600

    Commit

---
+++ Milestone-5/Module-25/index.js
@@ -0,0 +1,38 @@
+let pinkBtn = document.querySelector(".pinkBtn");
+pinkBtn.style.backgroundColor = "pink";
+pinkBtn.style.color = "white";
+pinkBtn.style.border = "none";
+pinkBtn.style.padding = "10px";
+pinkBtn.style.borderRadius = "15px";
+pinkBtn.style.cursor = "pointer";
+pinkBtn.style.fontSize = "20px";
+pinkBtn.style.fontWeight = "bolder";
+pinkBtn.addEventListener("click", function () {
+    document.body.style.backgroundColor = "pink";
+})
+
+let blueBtn = document.querySelector(".blueBtn");
+blueBtn.style.backgroundColor = "blue";
+blueBtn.style.color = "white";
+blueBtn.style.border = "none";
+blueBtn.style.padding = "10px";
+blueBtn.style.borderRadius = "15px";
+blueBtn.style.cursor = "pointer";
+blueBtn.style.fontSize = "20px";
+blueBtn.style.fontWeight = "bolder";
+blueBtn.addEventListener("click", function () {
+    document.body.style.backgroundColor = "blue";
+})
+
+let greenBtn = document.querySelector(".greenBtn");
+greenBtn.style.backgroundColor = "green";
+greenBtn.style.color = "white";
+greenBtn.style.border = "none";
+greenBtn.style.padding = "10px";
+greenBtn.style.borderRadius = "15px";
+greenBtn.style.cursor = "pointer";
+greenBtn.style.fontSize = "20px";
+greenBtn.style.fontWeight = "bolder";
+greenBtn.addEventListener("click", function () {
+    document.body.style.backgroundColor = "green";
+})

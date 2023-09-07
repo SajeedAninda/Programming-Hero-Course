1a550b09e474a5d6660c7ae8cf7d8968570c6c43
SajeedAninda <sajaninda@gmail.com>
2023-09-07 14:04:55 +0600

    Commit

--- Milestone-7/Module-38/first-react-install/src/App.jsx
+++ Milestone-7/Module-38/first-react-install/src/App.jsx
@@ -7,19 +7,34 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <MyName></MyName>
+      <Sisters name="Sanjeeda Enayet Ananna" age={17} height="4'11''" clgName="Noor Mohammad" weight={53}></Sisters>
+      <Sisters name="Tasmiyah Enayet Arthi" age={12} height="5'04''" clgName="Girls Ideal" weight={42}></Sisters>
+      <Sisters></Sisters>
     </>
   )
 }
-let MyName = () => {
-  let firstName = "Sajeed";
-  let lastName = "Aninda";
-  let age = 23;
+
+let Sisters =({name="",age=0,height=0,clgName="",weight=0}) =>{
+  let sistersStyle={
+    border: "2px solid red",
+    fontSize:"18px",
+    borderRadius:"20px",
+    padding:"20px",
+    marginTop:"15px",
+    width:"650px"
+  }
   return (
-    <div>
-      <h3>Hi i am {firstName}. My full name is {firstName} {lastName}. Very nice to meet you. I am {age} years old</h3>
+    <div style={sistersStyle}>
+    <h2>Name: {name}</h2>
+    <h2>Age: {age}</h2>
+    <h2>Height: {height}</h2>
+    <h2>Instituition Name: {clgName}</h2>
+    <h2>Weight: {weight}</h2>
+
     </div>
-  )
+    )
 }
 
 export default App
+
+// http://localhost:5173/

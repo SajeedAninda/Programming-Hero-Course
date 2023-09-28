f918082a0ad37cf1f52829cbbc8f1760e77dc623
SajeedAninda <sajaninda@gmail.com>
2023-09-29 01:29:49 +0600

    Commit

---
+++ Milestone-9/firebase-practice-2/src/Register.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "./firebase.config";
+const auth = getAuth(app);
+
+const Register = () => {
+    let [loginError, setLoginError] = useState("");
+    let [registerSuccess, setRegisterSuccess] = useState("");
+
+    let handleForm = (e) => {
+        e.preventDefault();
+        let email = e.target.email.value;
+        let password = e.target.password.value;
+        if (password.length <= 5) {
+            return alert("Password Should be more than 6 letters");
+        }
+        setLoginError("");
+        setRegisterSuccess("");
+
+
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                setRegisterSuccess(user);
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                console.log(error);
+                setLoginError(error.message);
+            });
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register Now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <form onSubmit={handleForm}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name='email' required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name='password' required className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                            {
+                                registerSuccess && <p className='text-green-400 text-xl mt-2'>Registration Succesful</p>
+                            }
+                            {
+                                loginError && <p className='text-red-600 text-xl mt-2'>{loginError}</p>
+                            }
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
---
+++ Milestone-9/firebase-practice-2/src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2inYYUYTdd2-Avy_QOVCXvYq3ZaQ7qVE",
+  authDomain: "fir-practice-2-40afb.firebaseapp.com",
+  projectId: "fir-practice-2-40afb",
+  storageBucket: "fir-practice-2-40afb.appspot.com",
+  messagingSenderId: "377583640914",
+  appId: "1:377583640914:web:b09a3470f1fc02384a5a8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
